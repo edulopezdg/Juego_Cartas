a6d132ccf55dfcae3fa8a627a2dc9f04c5bf6886
Edu Lopez Code <edulopez.dg@gmail.com>
2024-03-21 19:41:17 -0300

    Se creo la clase Juego y los metodos llenarMazo y mostrarBaraja

---
+++ src/juego_cartas/Juego.java
@@ -0,0 +1,38 @@
+
+package juego_cartas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Juego {
+    //Creamos 2 vectores
+    String[] numCartas = new String[]{"1","2","3","4","5","6","7","10","11","12"}; //Aca vamos a guardar los numeros
+    String[] palos = new String[]{"Oro","Espada","Basto","Copa"}; //Aca vamos a guardar los palos
+    
+    //Creamos 2 arraylist
+    List<String> mazo = new ArrayList<>();
+    
+    //Creamos un metodo para llenar el mazo
+    public void llenarMazo(){
+        for (String i : numCartas) {
+            for (String j : palos) {
+                mazo.add(i+j);
+            }
+        }
+    }
+    
+    //Creamos un metodo para mostrar el mazo
+    public void mostrarBaraja(){
+        int cont = 0;
+        for (String i : numCartas) {
+            cont ++;
+            System.out.println(i + "| ");
+            if (cont == 10) {
+                System.out.println("----------------");
+                cont = 0;
+                System.out.println("----");
+            }
+        }
+    }
+}
